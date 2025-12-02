4c67834e467a2a89e023b7fa4937c16db6338432
Jon Harmon <jon.harmon@atorusresearch.com>
2025-12-02 16:10:27 -0600

    Protect against testthat-version snapshot changes

--- tests/testthat/test-util-cli_logger_wrappers.R
+++ tests/testthat/test-util-cli_logger_wrappers.R
@@ -1,9 +1,61 @@
 # test-util-cli_logger_wrappers.R
 test_that("Use cli style messages via logger", {
-  expect_snapshot(LogMessage(level = "info", message = "cli style info", cli_detail = "h1"))
-  expect_snapshot(LogMessage(level = "info", message = "cli style info", cli_detail = "h2"))
-  expect_snapshot(LogMessage(level = "info", message = "cli style info", cli_detail = "h3"))
-  expect_snapshot(LogMessage(level = "info", message = "cli style info", cli_detail = "alert_success"))
-  expect_snapshot(tryCatch(LogMessage(level = "warn", message = "cli style warn")))
-  expect_error(LogMessage(level = "error", message = "cli style error"))
+  # Condense empty lines to stabilize snapshots between testthat versions.
+  cleaner <- function(txt) {
+    txt <- stringr::str_trim(txt)
+    txt[nzchar(txt)]
+  }
+
+  expect_snapshot(
+    {
+      LogMessage(
+        level = "info",
+        message = "cli style info",
+        cli_detail = "h1"
+      )
+    },
+    transform = cleaner
+  )
+  expect_snapshot(
+    {
+      LogMessage(
+        level = "info",
+        message = "cli style info",
+        cli_detail = "h2"
+      )
+    },
+    transform = cleaner
+  )
+  expect_snapshot(
+    {
+      LogMessage(
+        level = "info",
+        message = "cli style info",
+        cli_detail = "h3"
+      )
+    },
+    transform = cleaner
+  )
+  expect_snapshot(
+    {
+      LogMessage(
+        level = "info",
+        message = "cli style info",
+        cli_detail = "alert_success"
+      )
+    },
+    transform = cleaner
+  )
+  expect_snapshot(
+    {
+      tryCatch(LogMessage(
+        level = "warn",
+        message = "cli style warn"
+      ))
+    },
+    transform = cleaner
+  )
+  expect_error({
+    LogMessage(level = "error", message = "cli style error")
+  })
 })
