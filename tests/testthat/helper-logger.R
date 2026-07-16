ea72014d01510e1c813131857ed71402d447a26c
Jon Harmon <jon.harmon1@gilead.com>
2026-07-16 10:53:10 -0500

    Simplify test helpers

--- tests/testthat/helper-logger.R
+++ tests/testthat/helper-logger.R
@@ -1,5 +1,5 @@
 TestAtLogLevel <- function(chrLevel = "ERROR", envir = rlang::caller_env()) {
   old_level <- gsm.core::GetLogLevel()
-  withr::defer(gsm.core::SetLogger(old_level), envir = envir)
-  gsm.core::SetLogger(toupper(chrLevel))
+  withr::defer(gsm.core::SetLogLevel(old_level), envir = envir)
+  gsm.core::SetLogLevel(toupper(chrLevel))
 }
